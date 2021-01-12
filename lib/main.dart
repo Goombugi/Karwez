76a242aa01e06f8c8964a5af06655b83fd43240a
Goombugi <goombugi92@hotmail.com>
2021-01-12 09:35:46 +0300

    adding a drawer

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,110 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: Column(
+          children: <Widget>[
+            SizedBox(
+              height: 30,
+            ),
+            DrawerHeader(
+              child: Container(
+                  height: 142,
+                  width: MediaQuery.of(context).size.width,
+                  child: Image.asset(
+                    "assets/images/ten_news.png",
+                  )),
+              decoration: BoxDecoration(
+                color: Colors.transparent,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            GestureDetector(
+              onTap: () {
+                setState(() {
+
+                });
+                Navigator.of(context).pop();
+              },
+              child: Text(
+                'Profile',
+                style: TextStyle(
+                  fontFamily: 'Avenir',
+                  fontSize: 24,
+                  fontWeight: FontWeight.w700,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            SizedBox(
+              height: 45,
+            ),
+            GestureDetector(
+              onTap: () {},
+              child: Text(
+                'Settings',
+                style: TextStyle(
+                  fontFamily: 'Avenir',
+                  fontSize: 24,
+                  fontWeight: FontWeight.w700,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            SizedBox(
+              height: 45,
+            ),
+            Text(
+              'About',
+              style: TextStyle(
+                fontFamily: 'Avenir',
+                fontSize: 24,
+                fontWeight: FontWeight.w700,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(
+              height: 45,
+            ),
+            Text(
+              'Log Out',
+              style: TextStyle(
+                fontFamily: 'Avenir',
+                fontSize: 24,
+                fontWeight: FontWeight.w700,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(
+              height: 45,
+            ),
+            Expanded(
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Container(
+                    height: 65,
+                    width: MediaQuery.of(context).size.width,
+                    color: Colors.black,
+                    child: Center(
+                      child: Text(
+                        'v1.0.1',
+                        style: TextStyle(
+                          fontFamily: 'Avenir',
+                          fontSize: 20,
+                          color: const Color(0xffffffff),
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                ))
+          ],
+        ),
+
+      ),
+
 
       appBar: AppBar(
         elevation: 0.0,
@@ -66,6 +170,7 @@ class _HomeState extends State<Home> {
         ),
        
       ),
+
 
       body: ListView(
         padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
